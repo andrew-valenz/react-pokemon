9d4a0607d1539c00ed4c6b084d1d26ac36af3fa4
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2023-01-19 14:03:54 -0800

    useEffect hook working to fetch external data

--- src/hooks/pokemon.js
+++ src/hooks/pokemon.js
@@ -1,6 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { fetchInitalPokemon } from '../services/fetchPokemon.js';
 
 export function usePokemon() {
     const [pokemon, setPokemon] = useState([]);
-    
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await fetchInitialPokemon();
+            setPokemon(data);
+        };
+        fetchData;
+    }, []);
+    return pokemon;
 }
